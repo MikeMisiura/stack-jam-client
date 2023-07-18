e7678952b27cba5f973deebefd905629b26862fe
Mike Misiura <mikemisiura@gmail.com>
2023-07-18 01:58:21 -0600

    changed formatting

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,85 +3,24 @@ import { Button, Container, Ratio } from 'react-bootstrap';
 import CartContext from '../contexts/CartContext';
 import { useContext } from 'react';
 import { ICartProduct } from '../@types/product';
+import MessageContext from '../contexts/MessageContext';
 
 
 export default function Home() {
 
-    let { addToCart, cart, removeFromCart } = useContext(CartContext);
+    let { createMessage } = useContext(MessageContext);
 
 
-    function handleAddClick() {
+    function handleClick() {
 
-        let product: ICartProduct = {
-            "_id": "6488a8f38366b460d3e5acfe",
-            "productName": "Sample Product",
-            "color": "red",
-            "description": "description of Sample Product",
-            "price": 32,
-            "quantity": 2,
-            "groupCode": "sample",
-            "createdAt": "2023-06-13T17:35:47.486Z",
-            "updatedAt": "2023-06-13T17:35:47.486Z",
-            "__v": 0
-        }
-
-        addToCart(product)
-    }
-
-    function handleRemoveOneClick() {
-
-        let product: ICartProduct = {
-            "_id": "6488a8f38366b460d3e5acfe",
-            "productName": "Sample Product",
-            "color": "red",
-            "description": "description of Sample Product",
-            "price": 32,
-            "quantity": -1,
-            "groupCode": "sample",
-            "createdAt": "2023-06-13T17:35:47.486Z",
-            "updatedAt": "2023-06-13T17:35:47.486Z",
-            "__v": 0
-        }
-
-
-        addToCart(product)
-    }
-
-    function handleRemoveClick() {
-
-        let product: ICartProduct = {
-            "_id": "6488a8f38366b460d3e5acfe",
-            "productName": "Sample Product",
-            "color": "red",
-            "description": "description of Sample Product",
-            "price": 32,
-            "quantity": 1,
-            "groupCode": "sample",
-            "createdAt": "2023-06-13T17:35:47.486Z",
-            "updatedAt": "2023-06-13T17:35:47.486Z",
-            "__v": 0
-        }
-
-
-        removeFromCart(product)
-    }
-
-    function handleCartClick() {
-        if (cart[0]) {
-            console.log(cart[0].quantity)
-        }
-        
-        console.log(cart)
+        createMessage()
     }
 
     return (
         <div>
 
             <Container>
-            <Button className='m-2' onClick={handleAddClick}>add to cart</Button>
-            <Button className='m-2' onClick={handleRemoveOneClick}>remove 1 from cart</Button>
-            <Button className='m-2' onClick={handleRemoveClick}>remove from cart</Button>
-            <Button className='m-2' onClick={handleCartClick}>whats in cart</Button>
+            <Button className='m-2' onClick={handleClick}>Send Email</Button>
                 <h1>Welcome to the Stack Jam Website!</h1>
                 <Ratio className='mt-3' aspectRatio="16x9">
                     <iframe
