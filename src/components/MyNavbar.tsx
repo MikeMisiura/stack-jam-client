7d22a2d75f78cbe29463b9df72d9c845a58a0b40
Mike Misiura <mikemisiura@gmail.com>
2023-07-23 02:12:32 -0600

    etsy new tab

--- src/components/MyNavbar.tsx
+++ src/components/MyNavbar.tsx
@@ -30,7 +30,7 @@ export default function MyNavbar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Nav className="me-5">
         {/* <Nav.Link href="/home">Home</Nav.Link> */}
-        <Nav.Link href="https://www.etsy.com/shop/StackJam">Order now on Etsy!</Nav.Link>
+        <Nav.Link href="https://www.etsy.com/shop/StackJam" target="_blank">Order now on Etsy!</Nav.Link>
           
           {/* <Nav.Link href="/about-us">Our Story</Nav.Link> */}
           {/* <Nav.Link href="/contact-us">Contact Us</Nav.Link> */}
