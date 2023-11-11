62c47768f9eba571d389f9a7ed13b2d1bfbd1fe8
Mike Misiura <mikemisiura@gmail.com>
2023-11-10 21:50:59 -0700

    etsy to shopify

--- src/components/BuyNowButton.tsx
+++ src/components/BuyNowButton.tsx
@@ -1,25 +1,23 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import StackJamColors from '../theme/stackJamColors';
-import { Button, Image } from 'react-bootstrap';
-import "../theme/Global.css"
-import "./BuyNowButton.css"
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import StackJamColors from '../theme/stackJamColors'
+import { Button, Image } from 'react-bootstrap'
+import '../theme/Global.css'
+import './BuyNowButton.css'
 
 export default function BuyNowButton() {
-
-    return (
-        <Button
-            href='https://www.etsy.com/shop/StackJam'
-            target='blank'
-            className='
+	return (
+		<Button
+			href="https://96cbc5-3.myshopify.com/"
+			target="blank"
+			className="
                 buy-now-button 
                 rounded-4
                 shadow 
-            '
-        >
-            Buy Now!
-        </Button>
-    );
+            "
+		>
+			Buy Now!
+		</Button>
+	)
 }
-
