4f0264ffdf1ea1c02bef883020668712d6673d36
Mike Misiura <mikemisiura@gmail.com>
2023-12-08 22:59:17 -0700

    updated to 10 games in the rules

--- src/components/DownloadInstructionsButton.tsx
+++ src/components/DownloadInstructionsButton.tsx
@@ -6,16 +6,16 @@ export default function DownloadInstructionsButton() {
 	return (
 		<Button
 			// href="http://localhost:3001/pdf/dont-lose-your-head-rules.pdf"
-			href="https://s3.us-west-2.amazonaws.com/stackjam.us/Dont+Lose+Your+Head+Rules.pdf"
-			target="blank"
+			href='https://s3.us-west-2.amazonaws.com/stackjam.us/Dont%20Lose%20Your%20Head%20Rules.pdf'
+			target='blank'
 			// download
-			className="
+			className='
                 buy-now-button 
                 rounded-4
                 shadow 
-            "
+            '
 		>
-			<strong>Download Rules</strong> for 8 fun games
+			<strong>Download Rules</strong> for 10 fun games
 			<br />
 			and make up your own rules, too!
 		</Button>
